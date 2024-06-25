7363fbe4be8c0aaf6fa313a5e79541d7a5cee537
노수현 <140242522+kingsuhyeon@users.noreply.github.com>
2024-06-25 11:51:32 +0900

    추가 래퍼 클래스 - 오토 박싱 1, 2 코드 추가
    AutoboxingMain1, AutoboxingMain2.java 파일을 기록 보관용으로 업로드

---
+++ AutoboxingMain2.java
@@ -0,0 +1,12 @@
+package lang.wrapper;
+
+public class AutoboxingMain2 {
+    public static void main(String[] args) {
+        int value = 7;
+        Integer boxedValue = value; // 오토 박싱(Auto-boxing)
+
+        int unboxedValue = boxedValue; // 오토 언박싱(Auto-Unboxing)
+        System.out.println("boxedValue = " + boxedValue);
+        System.out.println("unboxedValue = " + unboxedValue);
+    }
+}
